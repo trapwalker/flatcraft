b39bb7e8484d55d4f808661641baa494b6e440d2
Алексей Постольский <AlexPos1989@gmail.com>
2015-12-29 16:29:13 +0300

    исправлена формула вычисления координат тайла

--- flatcraft/render.py
+++ flatcraft/render.py
@@ -4,15 +4,20 @@ from itertools import chain
 from tileid import Tileid
 
 
+def get_coord(tid, deep):
+    u"""Возвращает координаты тайла на плоскости"""
+    x, y, z = tid.xyz()
+    step = 2 ** (deep - z)
+    x0 = x * step
+    y0 = y * step
+    return [x0, y0, step]
+
+
 def test_tile_rect(x1, y1, x2, y2):
     def test(tile, deep):
-        x, y, z = tile.xyz()
-        delta = deep - z + 1
-
-        t_x1 = x * delta
-        t_y1 = y * delta
-        t_x2 = (x + 1) * delta
-        t_y2 = (y + 1) * delta
+        t_x1, t_y1, step = get_coord(tile, deep)
+        t_x2 = t_x1 + step
+        t_y2 = t_y1 + step
 
         if (x2 < t_x1) or (x1 > t_x2) or (y2 < t_y1) or (y1 > t_y2):
             return 0
