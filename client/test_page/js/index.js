51e7a7ec1398ee30e28cd589c90ae30c8205e174
svp <devnull@localhost>
2016-01-15 19:47:09 +0300

    test page add

---
+++ client/test_page/js/index.js
@@ -0,0 +1,104 @@
+DICT = ['red', 'yellow', 'blue', 'green', 'black', 'orange', 'purple'];
+BASE_COLOR = "rgb(155, 155, 255)";
+NC = 0xff; //Node Code 
+
+var data = [
+  [[NC, NC, NC, 6, 3, 3, 6, NC, 6, 3, 3, 6, NC, 6, 3, 3, 6, NC, 6, 3, 3, 6, NC, NC, 6, 3, 3, 6, NC, 6, 3, 3, 6, NC, 6,   3, 3, 6, NC, 6, 3, 3, 6, NC, NC, 6, 3, 3, 6, NC, 6, 3, 3, 6, NC, 6, 3, 3, 6, NC, 6, 3, 3, 6, NC, NC, 6, 3, 3, 6, NC,   6, 3, 3, 6, NC, 6, 3, 3, 6, NC, 6, 3, 3, 6],
+  [NC, NC, NC, 5, 7, 7, 5, NC, 5, 7, 7, 5, NC, 5, 7, 7, 5, NC, 5, 7, 7, 5, NC, NC, 5, 7, 7, 5, NC, 5, 7, 7, 5, NC, 5,   7, 7, 5, NC, 5, 7, 7, 5, NC, NC, 5, 7, 7, 5, NC, 5, 7, 7, 5, NC, 5, 7, 7, 5, NC, 5, 7, 7, 5, NC, NC, 5, 7, 7, 5, NC,   5, 7, 7, 5, NC, 5, 7, 7, 5, NC, 5, 7, 7, 5],
+  [NC, NC, NC, 1, 0, 0, 1, NC, 1, 0, 0, 1, NC, 1, 0, 0, 1, NC, 1, 0, 0, 1, NC, NC, 1, 0, 0, 1, NC, 1, 0, 0, 1, NC, 1,   0, 0, 1, NC, 1, 0, 0, 1, NC, NC, 1, 0, 0, 1, NC, 1, 0, 0, 1, NC, 1, 0, 0, 1, NC, 1, 0, 0, 1, NC, NC, 1, 0, 0, 1, NC,   1, 0, 0, 1, NC, 1, 0, 0, 1, NC, 1, 0, 0, 1],
+  [NC, NC, NC, 6, 4, 4, 6, NC, 6, 4, 4, 6, NC, 6, 4, 4, 6, NC, 6, 4, 4, 6, NC, NC, 6, 4, 4, 6, NC, 6, 4, 4, 6, NC, 6,   4, 4, 6, NC, 6, 4, 4, 6, NC, NC, 6, 4, 4, 6, NC, 6, 4, 4, 6, NC, 6, 4, 4, 6, NC, 6, 4, 4, 6, NC, NC, 6, 4, 4, 6, NC,   6, 4, 4, 6, NC, 6, 4, 4, 6, NC, 6, 4, 4, 6]],
+  [[NC, NC, NC, 6, 4, 4, 6, NC, 6, 4, 4, 6, NC, 6, 4, 4, 6, NC, 6, 4, 4, 6, NC, NC, 6, 4, 4, 6, NC, 6, 4, 4, 6, NC, 6,   4, 4, 6, NC, 6, 4, 4, 6, NC, NC, 6, 4, 4, 6, NC, 6, 4, 4, 6, NC, 6, 4, 4, 6, NC, 6, 4, 4, 6, NC, NC, 6, 4, 4, 6, NC,   6, 4, 4, 6, NC, 6, 4, 4, 6, NC, 6, 4, 4, 6],
+  [NC, NC, NC, 6, 3, 3, 6, NC, 6, 3, 3, 6, NC, 6, 3, 3, 6, NC, 6, 3, 3, 6, NC, NC, 6, 3, 3, 6, NC, 6, 3, 3, 6, NC, 6,   3, 3, 6, NC, 6, 3, 3, 6, NC, NC, 6, 3, 3, 6, NC, 6, 3, 3, 6, NC, 6, 3, 3, 6, NC, 6, 3, 3, 6, NC, NC, 6, 3, 3, 6, NC,   6, 3, 3, 6, NC, 6, 3, 3, 6, NC, 6, 3, 3, 6],
+  [NC, NC, NC, 5, 7, 7, 5, NC, 5, 7, 7, 5, NC, 5, 7, 7, 5, NC, 5, 7, 7, 5, NC, NC, 5, 7, 7, 5, NC, 5, 7, 7, 5, NC, 5,   7, 7, 5, NC, 5, 7, 7, 5, NC, NC, 5, 7, 7, 5, NC, 5, 7, 7, 5, NC, 5, 7, 7, 5, NC, 5, 7, 7, 5, NC, NC, 5, 7, 7, 5, NC,   5, 7, 7, 5, NC, 5, 7, 7, 5, NC, 5, 7, 7, 5],
+  [NC, NC, NC, 1, 0, 0, 1, NC, 1, 0, 0, 1, NC, 1, 0, 0, 1, NC, 1, 0, 0, 1, NC, NC, 1, 0, 0, 1, NC, 1, 0, 0, 1, NC, 1,   0, 0, 1, NC, 1, 0, 0, 1, NC, NC, 1, 0, 0, 1, NC, 1, 0, 0, 1, NC, 1, 0, 0, 1, NC, 1, 0, 0, 1, NC, NC, 1, 0, 0, 1, NC,   1, 0, 0, 1, NC, 1, 0, 0, 1, NC, 1, 0, 0, 1]],
+  [[NC, NC, NC, 1, 0, 0, 1, NC, 1, 0, 0, 1, NC, 1, 0, 0, 1, NC, 1, 0, 0, 1, NC, NC, 1, 0, 0, 1, NC, 1, 0, 0, 1, NC, 1,   0, 0, 1, NC, 1, 0, 0, 1, NC, NC, 1, 0, 0, 1, NC, 1, 0, 0, 1, NC, 1, 0, 0, 1, NC, 1, 0, 0, 1, NC, NC, 1, 0, 0, 1, NC,   1, 0, 0, 1, NC, 1, 0, 0, 1, NC, 1, 0, 0, 1],
+  [NC, NC, NC, 6, 4, 4, 6, NC, 6, 4, 4, 6, NC, 6, 4, 4, 6, NC, 6, 4, 4, 6, NC, NC, 6, 4, 4, 6, NC, 6, 4, 4, 6, NC, 6,   4, 4, 6, NC, 6, 4, 4, 6, NC, NC, 6, 4, 4, 6, NC, 6, 4, 4, 6, NC, 6, 4, 4, 6, NC, 6, 4, 4, 6, NC, NC, 6, 4, 4, 6, NC,   6, 4, 4, 6, NC, 6, 4, 4, 6, NC, 6, 4, 4, 6],
+  [NC, NC, NC, 6, 3, 3, 6, NC, 6, 3, 3, 6, NC, 6, 3, 3, 6, NC, 6, 3, 3, 6, NC, NC, 6, 3, 3, 6, NC, 6, 3, 3, 6, NC, 6,   3, 3, 6, NC, 6, 3, 3, 6, NC, NC, 6, 3, 3, 6, NC, 6, 3, 3, 6, NC, 6, 3, 3, 6, NC, 6, 3, 3, 6, NC, NC, 6, 3, 3, 6, NC,   6, 3, 3, 6, NC, 6, 3, 3, 6, NC, 6, 3, 3, 6],
+  [NC, NC, NC, 5, 7, 7, 5, NC, 5, 7, 7, 5, NC, 5, 7, 7, 5, NC, 5, 7, 7, 5, NC, NC, 5, 7, 7, 5, NC, 5, 7, 7, 5, NC, 5,   7, 7, 5, NC, 5, 7, 7, 5, NC, NC, 5, 7, 7, 5, NC, 5, 7, 7, 5, NC, 5, 7, 7, 5, NC, 5, 7, 7, 5, NC, NC, 5, 7, 7, 5, NC,   5, 7, 7, 5, NC, 5, 7, 7, 5, NC, 5, 7, 7, 5]],
+  [[NC, NC, NC, 5, 7, 7, 5, NC, 5, 7, 7, 5, NC, 5, 7, 7, 5, NC, 5, 7, 7, 5, NC, NC, 5, 7, 7, 5, NC, 5, 7, 7, 5, NC, 5,   7, 7, 5, NC, 5, 7, 7, 5, NC, NC, 5, 7, 7, 5, NC, 5, 7, 7, 5, NC, 5, 7, 7, 5, NC, 5, 7, 7, 5, NC, NC, 5, 7, 7, 5, NC,   5, 7, 7, 5, NC, 5, 7, 7, 5, NC, 5, 7, 7, 5],
+  [NC, NC, NC, 1, 0, 0, 1, NC, 1, 0, 0, 1, NC, 1, 0, 0, 1, NC, 1, 0, 0, 1, NC, NC, 1, 0, 0, 1, NC, 1, 0, 0, 1, NC, 1,   0, 0, 1, NC, 1, 0, 0, 1, NC, NC, 1, 0, 0, 1, NC, 1, 0, 0, 1, NC, 1, 0, 0, 1, NC, 1, 0, 0, 1, NC, NC, 1, 0, 0, 1, NC,   1, 0, 0, 1, NC, 1, 0, 0, 1, NC, 1, 0, 0, 1],
+  [NC, NC, NC, 6, 4, 4, 6, NC, 6, 4, 4, 6, NC, 6, 4, 4, 6, NC, 6, 4, 4, 6, NC, NC, 6, 4, 4, 6, NC, 6, 4, 4, 6, NC, 6,   4, 4, 6, NC, 6, 4, 4, 6, NC, NC, 6, 4, 4, 6, NC, 6, 4, 4, 6, NC, 6, 4, 4, 6, NC, 6, 4, 4, 6, NC, NC, 6, 4, 4, 6, NC,   6, 4, 4, 6, NC, 6, 4, 4, 6, NC, 6, 4, 4, 6],
+  [NC, NC, NC, 6, 3, 3, 6, NC, 6, 3, 3, 6, NC, 6, 3, 3, 6, NC, 6, 3, 3, 6, NC, NC, 6, 3, 3, 6, NC, 6, 3, 3, 6, NC, 6,   3, 3, 6, NC, 6, 3, 3, 6, NC, NC, 6, 3, 3, 6, NC, 6, 3, 3, 6, NC, 6, 3, 3, 6, NC, 6, 3, 3, 6, NC, NC, 6, 3, 3, 6, NC,   6, 3, 3, 6, NC, 6, 3, 3, 6, NC, 6, 3, 3, 6]]
+];
+
+function Iter(array) {
+  var idx = 0;
+  function iterator() {
+    idx += 1;
+    return array[idx - 1];
+  }
+  return iterator;
+}
+
+function print(text) {
+  var con = document.getElementById("console");
+  if (con) con.innerHTML += '<p>'+text+'</p>';
+}
+
+function load_tree(stream, callback, w, x, y) {
+  // todo: Пробрасывать глубину узла от корня
+  x = (x==undefined)?0:x;
+  y = (y==undefined)?0:y;
+  var node = stream();
+  if (node == NC) {
+    // todo: Добавить коллбэк on_node
+    w /= 2;
+    load_tree(stream, callback, w, x    , y    );
+    load_tree(stream, callback, w, x + w, y);
+    load_tree(stream, callback, w, x, y + w);
+    load_tree(stream, callback, w, x + w, y + w);    
+  }
+  else {
+    // todo: Переиеновать коллбэк в on_leaf
+    callback(node, w, x, y);
+  }
+}
+
+function fillCanvas(ctx, w, data) 
+{
+  function leaf_func(color, w, x, y) 
+  {
+    print(color + ", [" + x + ", " + y + "], " + w);
+    rect(x, y, w, DICT[color - 1])
+  }
+  
+  function rect(x1, y1, w, color) 
+  {
+    color = color || BASE_COLOR;
+    ctx.fillStyle = color; 
+    ctx.fillRect(x1, y1, w, w);
+  }
+  
+  rect(0, 0, w);
+  load_tree(Iter(data), leaf_func, w, 0, 0);  
+}
+
+var container = [];
+for(i = 0; i < 4; i++)
+{
+  var row = [];
+  for(j = 0; j < 4; j++)
+  {
+    var canvas = document.createElement('canvas');
+    canvas.width  = 256;
+    canvas.height = 256;
+    fillCanvas(canvas.getContext('2d'), 256, data[i][j]);
+    row.push(canvas);
+  }
+  container.push(row);
+}
+
+var x = 0;
+
+document.getElementById('render').onclick = function()
+{
+  ctx = document.getElementById('render').getContext('2d');
+  ctx.drawImage(container[x][0], 0, 0);
+  ctx.drawImage(container[x][1], 256, 0);
+  ctx.drawImage(container[x][2], 0, 256);
+  ctx.drawImage(container[x][3], 256, 256);
+  if (x < 3) {
+    x++;  
+  }
+  else x = 0;
+}
