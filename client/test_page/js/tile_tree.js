75af02c27cf22d41efaec69d94ea2a0ae183115a
svp <devnull@localhost>
2016-01-18 18:33:07 +0300

    tree: TileCache object add

--- client/test_page/js/tile_tree.js
+++ client/test_page/js/tile_tree.js
@@ -1,8 +1,9 @@
 ﻿
 function load_tree(stream, callback, ctx, w, x, y) {
   // todo: Пробрасывать глубину узла от корня
-  x = (x==undefined)?0:x;
-  y = (y==undefined)?0:y;
+  x = (x===undefined)?0:x;
+  y = (y===undefined)?0:y;
+  w = (w===undefined)?256:w;  // todo: Вынести размер тайла в константы
   var node = stream();
   if (node == NC) {
     // todo: Добавить коллбэк on_node
@@ -16,4 +17,40 @@ function load_tree(stream, callback, ctx, w, x, y) {
     // todo: Переиеновать коллбэк в on_leaf
     callback(ctx, node, w, x, y);
   }
+}
+
+
+function TileCache(src) {
+  src = (src===undefined)?{}:src;
+
+  var obj = {tiles: src}
+
+  obj.makeKey = (function(x, y) {
+    return x + ':' + y;
+  })
+
+  obj.onLoad() = (function(x, y) {
+    // todo: realize
+    return {data: '.'}
+  })
+
+  obj.getCanvas = (function(x, y) {
+    var key = obj.makeKey(x, y);
+    var tile = this.tiles[key];
+    if (tile === undefined) {
+      tile = this.onLoad(x, y);
+      this.tiles[key] = tile;
+    }
+    var canvas = tile.canvas;
+    if (canvas === undefined) {
+      canvas = document.createElement('canvas');  // todo: Вынести размер тайла в константы
+      canvas.width = 256;
+      canvas.height = 256;
+      var ctx = canvas.getContext('2d');
+      load_tree(tile.data, leafFunction, ctx);  // Перенести сюда leafFunction
+    }
+    return canvas;
+    
+  })
+
 }
