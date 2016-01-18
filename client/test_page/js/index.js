2afa88b29489fb6b281f80ae2bf57518e782fca7
Алексей Постольский <AlexPos1989@gmail.com>
2016-01-18 19:58:29 +0300

    client: Добавлен ресайз окна и начальная отрисовка.

--- client/test_page/js/index.js
+++ client/test_page/js/index.js
@@ -7,15 +7,25 @@
   function init() {
     workfield = document.getElementById('workfield');
     main_canvas = document.getElementById('render');
+    main_canvas.height = workfield.clientHeight;
+    main_canvas.width = workfield.clientWidth;
     main_ctx = main_canvas.getContext('2d');
 
+    repaint();
+
     function repaint() {
-      // todo: realize
+      //Тестовые данные для отображения:
+      main_ctx.fillStyle = BASE_COLOR;
+      main_ctx.fillRect(0, 0, main_canvas.width, main_canvas.height);
+      main_ctx.fillStyle = "black";
+      main_ctx.fillRect(0, 0, 128, 128);
     }
 
-    function resize(e) {
-      // todo: realize
-      repaint(); // ?
+    window.onresize = function resize()
+    {
+      main_canvas.height = workfield.clientHeight;
+      main_canvas.width = workfield.clientWidth;
+      repaint();
     }
   }
 
