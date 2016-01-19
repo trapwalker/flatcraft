d1ca81b5c1feb17b9b53faf2ee1e3cbe48b95e29
svp <devnull@localhost>
2016-01-19 17:14:29 +0300

    client/render: some fix

--- client/test_page/js/index.js
+++ client/test_page/js/index.js
@@ -11,14 +11,15 @@
     main_canvas.height = workfield.clientHeight;
     main_canvas.width = workfield.clientWidth;
     main_ctx = main_canvas.getContext('2d');
-    cache = TileCache('TEST_IMG_DATA');
+    cache = TileCache('qwe');
 
     function repaint() {
       //Тестовые данные для отображения:
       main_ctx.fillStyle = BASE_COLOR;
       main_ctx.fillRect(0, 0, main_canvas.width, main_canvas.height);
-      main_ctx.drawImage(cache.getCanvas(0, 0), 0, 0);
-      window.requestAnimationFrame(repaint)
+      var tile = cache.getCanvas(0, 0);
+      main_ctx.drawImage(tile, 0, 0);
+      window.requestAnimationFrame(repaint);
     }
 
     window.onresize = function resize()
