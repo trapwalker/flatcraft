69377ac8281be68fd167ae58423c7a32eaf59d4e
svp <devnull@localhost>
2016-01-21 14:57:02 +0300

    client/tools: print fix

--- client/test_page/js/tools.js
+++ client/test_page/js/tools.js
@@ -4,25 +4,26 @@ function Iter(array) {
   function iterator() {
     idx += 1;
     return array[idx - 1];
-  }
+  };
   return iterator;
-}
+};
+
 
 function print(text) {
   var con = document.getElementById("console");
   if (con) {
-    while (print.log_counter => print.LOG_ITEMS_LIMIT) {
+    while (print.log_counter >= print.LOG_ITEMS_LIMIT) {
       con.removeChild(con.firstChild);
       print.log_counter--;
-    }
+    };
     p = document.createElement('p');
     p.innerHTML = text;
     con.appendChild(p);
     print.log_counter++;
 
     con.scrollTop = con.scrollHeight;
-  }
-}
+  };
+};
 
 print.log_counter = 0;
 print.LOG_ITEMS_LIMIT = 100;
