aded3e2d5a1f3bd731316d3e5a8238a1f3ec1a68
svp <devnull@localhost>
2016-01-21 03:52:20 +0300

    client: c is vector

--- client/test_page/js/index.js
+++ client/test_page/js/index.js
@@ -4,19 +4,18 @@
   var main_canvas;
   var main_ctx;
   var cache;
-  var cx;
-  var cy;
+  var c;
   var movement_flag;
 
   function init() {
+    c = V(43.5 * CHUNK_SIZE, 31.5 * CHUNK_SIZE);
+
     workfield = document.getElementById('workfield');
     main_canvas = document.getElementById('render');
     main_canvas.height = workfield.clientHeight;
     main_canvas.width = workfield.clientWidth;
     main_ctx = main_canvas.getContext('2d');
     movement_flag = 0;
-    cx = 43 * CHUNK_SIZE;
-    cy = 31 * CHUNK_SIZE;
     cache = TileCache(TILES_AS_TREE);
     main_ctx.fillStyle = BASE_COLOR;
     //main_ctx.strokeStyle = "black";
@@ -27,15 +26,17 @@
     function repaint() {
       //Тестовые данные для отображения:
       main_ctx.fillRect(0, 0, main_canvas.width, main_canvas.height);
-      tx = Math.floor(cx / CHUNK_SIZE);
-      ty = Math.floor(cy / CHUNK_SIZE);
+
+      
+      tx = Math.floor(c.x / CHUNK_SIZE);
+      ty = Math.floor(c.y / CHUNK_SIZE);
       for (var i = ty - Math.ceil(main_canvas.height / CHUNK_SIZE / 2); i <= ty + Math.ceil(main_canvas.height / CHUNK_SIZE / 2); i++) {
       for (var j = tx - Math.ceil(main_canvas.width / CHUNK_SIZE / 2); j <= tx + Math.ceil(main_canvas.width / CHUNK_SIZE / 2); j++) {
           var tile = cache.getCanvas(j, i);
           main_ctx.drawImage(
               tile,
-              j * CHUNK_SIZE - cx + main_canvas.width / 2,
-              i * CHUNK_SIZE - cy + main_canvas.height / 2
+              j * CHUNK_SIZE - c.x + main_canvas.width / 2,
+              i * CHUNK_SIZE - c.y + main_canvas.height / 2
           );
           /*if (DEBUG_MOD) {
             main_ctx.rect();
@@ -57,8 +58,8 @@
 
     document.addEventListener('mousemove', function(e) {
       if(movement_flag) {
-        cx += old_x - e.pageX;
-        cy += old_y - e.pageY;
+        c.x += old_x - e.pageX;
+        c.y += old_y - e.pageY;
         old_x = e.pageX;
         old_y = e.pageY;
       }
@@ -66,11 +67,6 @@
 
     document.addEventListener('mouseup', function() {
       movement_flag = 0;
-      print('left_y = ' + (ty - Math.ceil(main_canvas.height / CHUNK_SIZE / 2)));
-      print('right_y = ' + (ty + Math.ceil(main_canvas.height / CHUNK_SIZE / 2)));
-      print('left_x = ' + (tx - Math.ceil(main_canvas.width / CHUNK_SIZE / 2)));
-      print('right_x = ' + (tx + Math.ceil(main_canvas.height / CHUNK_SIZE / 2)));
-      print('-----------------------------------');
     });
 
     window.onresize = function resize() {
