9125275d4a01adf1c15556faee5d9bbf9e983b94
svp <devnull@localhost>
2016-01-25 18:02:56 +0300

    client/map: init new module

---
+++ client/test_page/js/map.js
@@ -0,0 +1,48 @@
+﻿
+
+function Tile(x, y, z, kind) {
+  this.x = x;
+  this.y = y;
+  this.z = z;
+  this.kind = kind;
+  this.state = null;
+
+  this.src = null;
+  this.value = null;
+}
+
+Tile.prototype.draw = function(map) {
+  ctx = map.ctx;
+
+  
+};
+
+/// <apWidget /////////////////////////////////////////////////////////////////////////////////////
+function MapWidget(container_id, tile_size) {
+  this.tile_size = tile_size || 256;
+
+  this.container = document.getElementById(container_id);  // todo: throw error if not found
+  this.canvas = document.createElement('canvas');
+  this.ctx = this.canvas.getContext('2d');
+
+  this.container.appendChild(this.canvas);
+
+  this.c = V(0, 0);
+  this.is_scrolling_now = false;
+
+  this.cache = TileCache(TILES_AS_TREE);
+
+    
+
+}
+
+MapWidget.prototype.locate = function(x, y) {
+  this.c = (y === undefined)? x : V(x, y);
+
+};
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+m = new MapWidget();
+m.locate(43.5 * this.tile_size, 31.5 * this.tile_size);
