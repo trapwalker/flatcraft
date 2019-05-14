4e2f3a70e8042bcc438149936d5181b87fe59a76
svp <svpmailbox@gmail.com>
2019-05-14 18:57:19 +0300

    server/model: Motion model refactoring

--- world/motion.py
+++ world/motion.py
@@ -22,6 +22,7 @@ class Motion(NamedTuple):
         a: complex = m1.p0 - m2.p0 - m1.v * m1.t0 + m2.v * m2.t0
         b: complex = m1.v - m2.v
         # | a + t*b | < r
+        # aa * t ** 2 + bb * t + cc == 0
         aa = b.real ** 2 + b.imag ** 2
         bb = 2 * (a.real * b.real + a.imag * b.imag)
         cc = a.real ** 2 + a.imag ** 2 - r ** 2
@@ -30,12 +31,13 @@ class Motion(NamedTuple):
             # No motion double intersects with radius r
             return
 
-        dd = sqrt(dd)
+        dd = dd ** 0.5
 
         early_time = max(m1.t0, m2.t0)
         t1 = (-bb - dd) / (2 * aa)
         t2 = (-bb + dd) / (2 * aa)
-        # Deop
+        # Drop the time if it was earlier than most late starting of motion
+        # we are do not known what was happens before then late motion starts
         return (
             t1 if t1 >= early_time else None,
             t2 if t2 >= early_time else None,
