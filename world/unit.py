2ed61e4a858031e84286171f666b763ab2386cbd
svp <svpmailbox@gmail.com>
2019-05-14 11:30:19 +0300

    Motion model vectorization FIX

--- world/unit.py
+++ world/unit.py
@@ -14,24 +14,26 @@ class Unit:
 class Motion:
     time: float
     start_position: Position
-    direction: float
-    velocity: float
+    velocity: Vector
 
-    def __init__(self, time, start_position, direction, velocity):
+    def __init__(self, time, start_position, velocity=Vector(0)):
         self.time = time
         self.start_position = start_position
-        self.direction = direction
         self.velocity = velocity
 
-    def to_time(self, t, new_direction: float = None, new_velocity: float = None) -> 'Motion':
-        old_direction = self.direction
+    def to_time(self, t, new_velocity: Vector = None) -> 'Motion':
         old_velocity = self.velocity
         return Motion(
             time=t,
-            start_position=self.start_position + Vector.from_polar(old_velocity * (t - self.time), old_direction),
-            direction=old_direction if new_direction is None else new_direction,
+            start_position=self.start_position + old_velocity * (t - self.time),
             velocity=old_velocity if new_velocity is None else new_velocity,
         )
+
+    def intersect(self, m2: 'Motion', r: float):
+        s = self.start_position - m2.start_position
+        v = self.velocity - m2.velocity
+        # TODO: calc intersection in continuum
+
 
 
 class Mobile(Unit):
